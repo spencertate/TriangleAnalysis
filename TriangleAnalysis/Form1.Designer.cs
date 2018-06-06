1650f26f4d1b2985cf3c76878cdd27368b8cb076
Spencer R. Tate <shoit718@go.byuh.edu>
2018-06-05 20:00:14 -0600

    Initial Commit
    Fully functioning

---
+++ TriangleAnalysis/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace TriangleAnalysis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.aSideInput = new System.Windows.Forms.TextBox();
+            this.bSideInput = new System.Windows.Forms.TextBox();
+            this.cSideInput = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSubmit);
+            this.groupBox1.Controls.Add(this.cSideInput);
+            this.groupBox1.Controls.Add(this.bSideInput);
+            this.groupBox1.Controls.Add(this.aSideInput);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(298, 294);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Side A Length:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Side B Length:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Side C Length:";
+            // 
+            // aSideInput
+            // 
+            this.aSideInput.Location = new System.Drawing.Point(138, 45);
+            this.aSideInput.Name = "aSideInput";
+            this.aSideInput.Size = new System.Drawing.Size(130, 22);
+            this.aSideInput.TabIndex = 3;
+            // 
+            // bSideInput
+            // 
+            this.bSideInput.Location = new System.Drawing.Point(138, 101);
+            this.bSideInput.Name = "bSideInput";
+            this.bSideInput.Size = new System.Drawing.Size(130, 22);
+            this.bSideInput.TabIndex = 4;
+            // 
+            // cSideInput
+            // 
+            this.cSideInput.Location = new System.Drawing.Point(138, 159);
+            this.cSideInput.Name = "cSideInput";
+            this.cSideInput.Size = new System.Drawing.Size(130, 22);
+            this.cSideInput.TabIndex = 5;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(87, 214);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(104, 55);
+            this.btnSubmit.TabIndex = 6;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(323, 317);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Triangle Analyzer";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox cSideInput;
+        private System.Windows.Forms.TextBox bSideInput;
+        private System.Windows.Forms.TextBox aSideInput;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TriangleAnalysis/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TriangleAnalysis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //Begin text preparation
+            String message = "";
+
+            //Pull data from text boxes
+            string aRaw = aSideInput.Text;
+            string bRaw = bSideInput.Text;
+            string cRaw = cSideInput.Text;
+
+            int aInt = 0, bInt = 0, cInt = 0;
+
+            bool valid = true;
+            
+            //Side A input validation
+            if(aRaw.All(c => char.IsDigit(c)) && aRaw != "")
+            {
+                aInt = Convert.ToInt32(aRaw);
+            }
+            else
+            {
+                message += "\nEnsure Side A is a valid integer.";
+                valid = false;
+            }
+
+            //Side B input validation
+            if (bRaw.All(c => char.IsDigit(c)) && bRaw != "")
+            {
+                bInt = Convert.ToInt32(bRaw);
+            }
+            else
+            {
+                message += "\nEnsure Side B is a valid integer.";
+                valid = false;
+            }
+
+            //Side C input validation
+            if (cRaw.All(c => char.IsDigit(c)) && cRaw != "")
+            {
+                cInt = Convert.ToInt32(cRaw);
+            }
+            else
+            {
+                message += "\nEnsure Side C is a valid integer.";
+                valid = false;
+            }
+            
+            //Triangle shape validation
+            if(valid && !((aInt + bInt > cInt) && (bInt + cInt > aInt) && (cInt + aInt > bInt)))
+            {
+                message += "\nGiven sides do not make a triangle." + aInt + " " + bInt + " " + cInt;
+                valid = false;
+            }
+
+            //If not valid, create message box showing errors
+            if(!valid)
+            {
+                MessageBox.Show("Invalid Triangle: " + message);
+                return;
+            }
+
+            //Begin valid triangle report
+            message = "Inputs make a valid triangle of type:";
+
+            //Test for an equilateral triangle
+            if(aInt == bInt && bInt == cInt)
+            {
+                message += "\nEquilateral Triangle";
+            }
+            else
+            {
+                //Test for an isosceles triangle
+                if(aInt == bInt || bInt == cInt || cInt == aInt)
+                {
+                    message += "\nIsosceles Triangle";
+                }
+                //Test for a right triangle
+                if(isRight(aInt, bInt, cInt) || isRight(bInt, cInt, aInt) || isRight(cInt, aInt, bInt))
+                {
+                    message += "\nRight Triangle";
+                }
+            }
+
+            //If not any of the three above, type is regular
+            if(message == "Inputs make a valid triangle of type:")
+            {
+                message += "\nRegular Triangle";
+            }
+
+            //Show output
+            MessageBox.Show(message);
+        }
+
+        //Encapsulation of Pythagorean Theorem
+        private bool isRight(int a, int b, int c)
+        {
+            return ((a * a) + (b * b)) == (c * c);
+        }
+    }
+}
